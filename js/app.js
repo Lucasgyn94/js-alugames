0dd26deaa77e0dacf0f41c84b2beb85d7a2b1fae
Lucas Ferreira Da Silva <needslucas944@gmail.com>
2025-10-26 13:35:05 -0300

    refatoração - recebendo game por id para mudança de status

--- js/app.js
+++ js/app.js
@@ -1,22 +1,15 @@
-const alterarStatus = (gameAction) => {
-    gameAction.addEventListener("click", () => {
-        if (gameAction.innerHTML === "Alugar") {
-            gameAction.innerHTML = "Devolver";
-            gameAction.classList.add("dashboard__item__button--return");
-            return;
-        } else if (gameAction.innerHTML === "Devolver") {
-            gameAction.innerHTML = "Alugar";
-            gameAction.classList.remove("dashboard__item__button--return");
-            return;
-        }
+const alterarStatus = (id) => {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let gameImagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
 
-    });
+    if (gameImagem.classList.contains("dashboard__item__img--rented")) {
+        gameImagem.classList.remove("dashboard__item__img--rented");
+        botao.classList.remove("dashboard__item__button--return");
+        botao.innerHTML = "Alugar";
+    } else {
+        gameImagem.classList.add("dashboard__item__img--rented");
+        botao.classList.add("dashboard__item__button--return");
+        botao.innerHTML = "Devolver";
+    }
 }
-
-let gameUmAction = document.getElementById("game-1-action");
-let gameDoisAction = document.getElementById("game-2-action");
-let gameTresAction = document.getElementById("game-3-action");
-
-alterarStatus(gameUmAction);
-alterarStatus(gameDoisAction);
-alterarStatus(gameTresAction);
